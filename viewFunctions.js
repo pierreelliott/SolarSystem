92c831d97b1fd05f3c8fe0c956c2c2c07c4dc6d0
Pierre-Elliott Thiboud <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2017-02-11 17:49:02 +0100

    Fly controls + Code dispatch
    Fly controls added, doesn't work well for now (translate only on X and Z-axis)
    I tried to dispatch some code into other files but there are some problems, everything's works but code isn't very "good-looking"

---
+++ viewFunctions.js
@@ -0,0 +1,29 @@
+function onWindowResize( event ) {
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+}
+
+function cameraZoom(e) {
+	//console.log(e);
+    e.preventDefault();
+	var move = e.deltaY/2, moveMin = 2, moveMax = 110;
+	if((camera.position.z + move) > moveMin && (camera.position.z + move) < moveMax) {
+		camera.position.z += move;
+	}
+}
+
+function cameraRotate(e) {
+	//console.log(e);
+    e.preventDefault();
+	camera.position.z += e.deltaY;
+    /*if(e.deltaY < 0)
+    { camera.position.z -= 1; }
+    else
+    { camera.position.z += 1; }*/
+}
+
+function leftClickOnView(e) {
+	e.preventDefault();
+	document.getElementById('mainView').addEventListener("click", leftClickOnView );
+}
