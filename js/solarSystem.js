b0bcecb621181183723fcef3d2618f68365e3f04
Unknown <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2017-12-11 20:57:21 +0100

    Minor change to the camera
    I will need to understand a little bit more how it works...

--- js/solarSystem.js
+++ js/solarSystem.js
@@ -115,7 +115,7 @@ window.onload = function() {
 
 	function init() {
 		scene = new THREE.Scene();
-		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 		renderer = new THREE.WebGLRenderer();
 		renderer.setSize( window.innerWidth, window.innerHeight );
